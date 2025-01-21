a5b1b4865b5d1588ce93acfb157899e09eb3ab65
josue fuentes <josue.chaqui@gmail.com>
2025-01-21 16:50:09 -0600

    Add DTOs for game creation and retrieval with sample data

---
+++ Dtos/GameDto.cs
@@ -0,0 +1,8 @@
+namespace GameStore.Api.Dtos;
+
+public record class GameDto(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate) : CreateGameDto(Name, Genre, Price, ReleaseDate)
+{
+    public GameDto(CreateGameDto game, int id) : this(id, game.Name, game.Genre, game.Price, game.ReleaseDate)
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using GameStore.Api.Dtos;
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        const string GetGameEndpoint = "GetGameById";
+        List<GameDto> games = new()
+{
+    new GameDto(1, "GTA V", "Action", 99.99m, new DateOnly(2013, 9, 17)),
+    new GameDto(2, "FIFA 22", "Sport", 199.99m, new DateOnly(2021, 10, 1)),
+    new GameDto(3, "Cyberpunk 2077", "RPG", 199.99m, new DateOnly(2020, 12, 10))
+};
+
+        app.MapGet("/games", () => games);
+        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/games/{id}", (int id) =>
+            games.FirstOrDefault(g => g.Id == id) ?? new GameDto(0, "Not Found", "Not Found", 0, new DateOnly()))
+            .WithName(GetGameEndpoint);
+
+        app.MapPost("/games", (CreateGameDto game) =>
+        {
+            var newGame = new GameDto(game, games.Count + 1);
+            games.Add(newGame);
+            return Results.CreatedAtRoute(GetGameEndpoint, new { id = newGame.Id }, newGame);
+        });
+        app.Run();
+    }
+}
